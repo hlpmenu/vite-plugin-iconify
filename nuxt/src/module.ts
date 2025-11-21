bfbf0064b216ace052e92507e43b0f6a10c0fe85
oli <oli@hlmpn.dev>
2025-11-21 16:44:08 +0100

    update to nuxt module config

--- nuxt/src/module.ts
+++ nuxt/src/module.ts
@@ -1,15 +1,47 @@
-import { defineNuxtModule, addVitePlugin } from "@nuxt/kit";
+import { defineNuxtModule, addVitePlugin, addComponentExports } from "@nuxt/kit";
 import iconifyPlugin from "@hlmpn/vite-plugin-iconify";
-import type { NuxtModule } from "@nuxt/schema";
+import type { NuxtConfig as NuxtConfigBase } from "@nuxt/schema";
 
-const nuxtModule = defineNuxtModule<NuxtModule>({
+export interface IconifyNuxtModuleOptions {
+	/**
+		* Optional prefix applied before the component name (default: "").
+		* Example: "Iconify" => <IconifyIcon />.
+		*/
+	prefix?: string;
+}
+
+const nuxtModule = defineNuxtModule<IconifyNuxtModuleOptions>({
 	meta: {
 		name: "@hlmpn/vite-plugin-iconify/nuxt",
 		configKey: "iconify",
 	},
-	setup() {
+	defaults: {
+		prefix: "",
+	},
+	setup(options) {
 		addVitePlugin(iconifyPlugin());
+
+		addComponentExports({
+			pascalName: "Icon",
+			kebabName: "Icon",
+			filePath: "@hlmpn/vite-plugin-iconify/vue",
+			prefix: options.prefix ?? "",
+			global: true,
+			mode: "all",
+			prefetch: false,
+			preload: false,
+		});
 	},
 });
 
 export default nuxtModule;
+
+declare module "@nuxt/schema" {
+	interface NuxtConfig extends NuxtConfigBase {
+		iconify?: IconifyNuxtModuleOptions;
+	}
+
+	interface NuxtOptions {
+		iconify?: IconifyNuxtModuleOptions;
+	}
+}
